326f39535ce66b6236068248cc749259782046ec
aidan mcintosh <aidanamcintosh@gmail.com>
2025-01-28 07:53:40 -0400

    Added gitignore to prevent env and __pycache__ from being pushed to repo

--- main.py
+++ main.py
@@ -16,12 +16,6 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-]
 
 client = httpx.AsyncClient(
     http2=True,
